c502a785ef45ba19d31a6bbb3d5582ea7d24cc32
高冷的旺泰 <617182106@qq.com>
2017-04-05 01:45:18 +0800

    旺泰似乎是完成任务了
    1. 修改了vNode数据结构：增添了int hostPNodeID代表分配到的物理机编号
    2. 修改了vList数据结构，添加了int taskState用于表示任务分配状态。
    3. 增添了程序正式执行前对任务按到来时间顺序排序
    4. 实现了最简单的子任务调度算法
    5. 木腿加油^_^

---
+++ test.h
@@ -0,0 +1,13 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+
+#define LOCALERROR -1
+#define TIME_STEP 1
+#include "generate.h"
+
+//void PerformTest(int taskNum, struct vList* list, int dis[][PN_MAX]);
+void sortByArriveTime(struct vList*  list, int taskNum);
+int * UsePNodeInOrder(int taskNum, int pNodeNum, struct vList* list, int dis[][PN_MAX], struct pNode** pNodeList);
+
+
+#endif // !_TEST_H_
